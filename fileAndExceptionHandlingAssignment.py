ef3d00461ca99d791dfb5a440889b715d9c97e88
Beatrice1911 <beatriceokereke19@gmail.com>
2025-04-30 23:26:52 +0100

    Update fileAndExceptionHandlingAssignment.py

--- fileAndExceptionHandlingAssignment.py
+++ fileAndExceptionHandlingAssignment.py
@@ -1,1 +1,10 @@
+with open("input.txt", "r") as file:
+    text = file.read()
+  
+upper_text = text.upper()
+output_content = f"{upper_text}"
+
+with open("output.txt", "w") as file:
+    file.write(output_content)
+
 
